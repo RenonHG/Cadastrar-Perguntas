747d2ce38215a1735d0043b2a9412df7d9ccdc58
RenonHG <141833668+RenonHG@users.noreply.github.com>
2023-11-28 11:50:33 -0300

    Initial commit

---
+++ Cadastrar Perguntas/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Cadastrar_Perguntas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxPergunta = new System.Windows.Forms.RichTextBox();
+            this.txbAlternativaA = new System.Windows.Forms.TextBox();
+            this.txbAlternativaB = new System.Windows.Forms.TextBox();
+            this.txbAlternativaC = new System.Windows.Forms.TextBox();
+            this.txbAlternativaD = new System.Windows.Forms.TextBox();
+            this.rbAlternativaCorretaA = new System.Windows.Forms.RadioButton();
+            this.rbAlternativaCorretaB = new System.Windows.Forms.RadioButton();
+            this.rbAlternativaCorretaC = new System.Windows.Forms.RadioButton();
+            this.rbAlternativaCorretaD = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtxPergunta
+            // 
+            this.rtxPergunta.Location = new System.Drawing.Point(12, 12);
+            this.rtxPergunta.Name = "rtxPergunta";
+            this.rtxPergunta.Size = new System.Drawing.Size(528, 120);
+            this.rtxPergunta.TabIndex = 0;
+            this.rtxPergunta.Text = "";
+            // 
+            // txbAlternativaA
+            // 
+            this.txbAlternativaA.Location = new System.Drawing.Point(52, 156);
+            this.txbAlternativaA.Name = "txbAlternativaA";
+            this.txbAlternativaA.Size = new System.Drawing.Size(252, 20);
+            this.txbAlternativaA.TabIndex = 1;
+            // 
+            // txbAlternativaB
+            // 
+            this.txbAlternativaB.Location = new System.Drawing.Point(52, 193);
+            this.txbAlternativaB.Name = "txbAlternativaB";
+            this.txbAlternativaB.Size = new System.Drawing.Size(252, 20);
+            this.txbAlternativaB.TabIndex = 2;
+            // 
+            // txbAlternativaC
+            // 
+            this.txbAlternativaC.Location = new System.Drawing.Point(52, 225);
+            this.txbAlternativaC.Name = "txbAlternativaC";
+            this.txbAlternativaC.Size = new System.Drawing.Size(252, 20);
+            this.txbAlternativaC.TabIndex = 3;
+            // 
+            // txbAlternativaD
+            // 
+            this.txbAlternativaD.Location = new System.Drawing.Point(52, 257);
+            this.txbAlternativaD.Name = "txbAlternativaD";
+            this.txbAlternativaD.Size = new System.Drawing.Size(252, 20);
+            this.txbAlternativaD.TabIndex = 4;
+            // 
+            // rbAlternativaCorretaA
+            // 
+            this.rbAlternativaCorretaA.AutoSize = true;
+            this.rbAlternativaCorretaA.Location = new System.Drawing.Point(12, 159);
+            this.rbAlternativaCorretaA.Name = "rbAlternativaCorretaA";
+            this.rbAlternativaCorretaA.Size = new System.Drawing.Size(14, 13);
+            this.rbAlternativaCorretaA.TabIndex = 5;
+            this.rbAlternativaCorretaA.TabStop = true;
+            this.rbAlternativaCorretaA.UseVisualStyleBackColor = true;
+            // 
+            // rbAlternativaCorretaB
+            // 
+            this.rbAlternativaCorretaB.AutoSize = true;
+            this.rbAlternativaCorretaB.Location = new System.Drawing.Point(12, 196);
+            this.rbAlternativaCorretaB.Name = "rbAlternativaCorretaB";
+            this.rbAlternativaCorretaB.Size = new System.Drawing.Size(14, 13);
+            this.rbAlternativaCorretaB.TabIndex = 6;
+            this.rbAlternativaCorretaB.TabStop = true;
+            this.rbAlternativaCorretaB.UseVisualStyleBackColor = true;
+            // 
+            // rbAlternativaCorretaC
+            // 
+            this.rbAlternativaCorretaC.AutoSize = true;
+            this.rbAlternativaCorretaC.Location = new System.Drawing.Point(12, 228);
+            this.rbAlternativaCorretaC.Name = "rbAlternativaCorretaC";
+            this.rbAlternativaCorretaC.Size = new System.Drawing.Size(14, 13);
+            this.rbAlternativaCorretaC.TabIndex = 7;
+            this.rbAlternativaCorretaC.TabStop = true;
+            this.rbAlternativaCorretaC.UseVisualStyleBackColor = true;
+            // 
+            // rbAlternativaCorretaD
+            // 
+            this.rbAlternativaCorretaD.AutoSize = true;
+            this.rbAlternativaCorretaD.Location = new System.Drawing.Point(12, 257);
+            this.rbAlternativaCorretaD.Name = "rbAlternativaCorretaD";
+            this.rbAlternativaCorretaD.Size = new System.Drawing.Size(14, 13);
+            this.rbAlternativaCorretaD.TabIndex = 8;
+            this.rbAlternativaCorretaD.TabStop = true;
+            this.rbAlternativaCorretaD.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "a)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "b)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "c)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "d)";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(34, 317);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 13;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 393);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbAlternativaCorretaD);
+            this.Controls.Add(this.rbAlternativaCorretaC);
+            this.Controls.Add(this.rbAlternativaCorretaB);
+            this.Controls.Add(this.rbAlternativaCorretaA);
+            this.Controls.Add(this.txbAlternativaD);
+            this.Controls.Add(this.txbAlternativaC);
+            this.Controls.Add(this.txbAlternativaB);
+            this.Controls.Add(this.txbAlternativaA);
+            this.Controls.Add(this.rtxPergunta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxPergunta;
+        private System.Windows.Forms.TextBox txbAlternativaA;
+        private System.Windows.Forms.TextBox txbAlternativaB;
+        private System.Windows.Forms.TextBox txbAlternativaC;
+        private System.Windows.Forms.TextBox txbAlternativaD;
+        private System.Windows.Forms.RadioButton rbAlternativaCorretaA;
+        private System.Windows.Forms.RadioButton rbAlternativaCorretaB;
+        private System.Windows.Forms.RadioButton rbAlternativaCorretaC;
+        private System.Windows.Forms.RadioButton rbAlternativaCorretaD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCadastrar;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cadastrar-Perguntas
+ 
